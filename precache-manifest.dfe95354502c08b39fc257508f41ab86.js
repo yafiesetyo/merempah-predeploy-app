b633dab9234de12bc103a5173ace3f498d2c60bb
yafiesetyo <yafiesetyo@gmail.com>
2020-08-14 15:06:54 +0700

    Updates

---
+++ precache-manifest.dfe95354502c08b39fc257508f41ab86.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e6a0930f5a443f90a084ae1a86bcdc",
+    "url": "/merempah-predeploy-app/index.html"
+  },
+  {
+    "revision": "8c259004091306baeb9b",
+    "url": "/merempah-predeploy-app/static/css/2.a3ef9e1d.chunk.css"
+  },
+  {
+    "revision": "c2bb5e4551d1b8d30b9b",
+    "url": "/merempah-predeploy-app/static/css/main.62d52ab5.chunk.css"
+  },
+  {
+    "revision": "8c259004091306baeb9b",
+    "url": "/merempah-predeploy-app/static/js/2.2cca97ef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/merempah-predeploy-app/static/js/2.2cca97ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2bb5e4551d1b8d30b9b",
+    "url": "/merempah-predeploy-app/static/js/main.509d8833.chunk.js"
+  },
+  {
+    "revision": "828c0eac78c7387e8901",
+    "url": "/merempah-predeploy-app/static/js/runtime-main.3b629f7c.js"
+  },
+  {
+    "revision": "d52fb060cac65344cbc656867ef54425",
+    "url": "/merempah-predeploy-app/static/media/hero1.d52fb060.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/merempah-predeploy-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/merempah-predeploy-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/merempah-predeploy-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/merempah-predeploy-app/static/media/slick.f97e3bbf.svg"
+  }
+]);
